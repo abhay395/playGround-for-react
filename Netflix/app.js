0cf8f228642c3996c85142315722dddfc4cb6bde
abhay395 <abhayparja90@gmail.com>
2023-10-13 12:14:03 +0530

    complete

--- Netflix/app.js
+++ Netflix/app.js
@@ -1,1 +1,8 @@
-import './image/Hero.jpg'
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        const section =  e.currentTarget.parentElement.parentElement
+        section.classList.toggle('active')
+    })
+})
